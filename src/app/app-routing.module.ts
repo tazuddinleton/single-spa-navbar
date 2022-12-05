4b5d3d0e0fc3f4b1407609e53d8f7207a78f8c9e
taz <tazuddinleton@gmail.com>
2022-12-05 13:14:36 +0600

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
   providers: [
-    {provide: APP_BASE_HREF, useValue: '/'}
+    {provide: APP_BASE_HREF, useValue: '/navbar'}
   ]
 })
 export class AppRoutingModule { }
